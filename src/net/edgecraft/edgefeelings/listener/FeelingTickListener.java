f0a6c7c08248a63f15d6a867f6503f89a5c20387
ase 34 <mail@fake.com>
2014-03-28 17:26:21 +0100

    Hunger Ausnahme

--- src/net/edgecraft/edgefeelings/listener/FeelingTickListener.java
+++ src/net/edgecraft/edgefeelings/listener/FeelingTickListener.java
@@ -3,6 +3,8 @@ package net.edgecraft.edgefeelings.listener;
 import net.edgecraft.edgefeelings.FeelingType;
 import net.edgecraft.edgefeelings.event.FeelingTickEvent;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,6 +14,15 @@ public class FeelingTickListener implements Listener {
     @EventHandler(priority = EventPriority.LOW)
     public void onFeelingTick(FeelingTickEvent ev) {
         FeelingType type = ev.getFeeling().getType();
+        Player player = Bukkit.getPlayerExact(ev.getUser().getUser().getName());
+        if (player == null) {
+            return;
+        }
+        
+        if (type == FeelingType.HUNGER) {
+            ev.getFeeling().setCurrentValue(player.getFoodLevel());
+            return;
+        }
         
         ev.getFeeling().addToCurrentValue(type.getDefaultChangement());
     }
